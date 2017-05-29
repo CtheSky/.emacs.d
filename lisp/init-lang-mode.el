f98a3249596b1967a63b17798ed4fa8f9bf0209f
CtheSky <cthesky@yeah.net>
2017-05-29 20:19:00 +0800

    add octave support

--- lisp/init-lang-mode.el
+++ lisp/init-lang-mode.el
@@ -1,3 +1,13 @@
+;; octave mode
+(setq auto-mode-alist
+      (cons '("\\.m$" . octave-mode) auto-mode-alist))
+(add-hook 'octave-mode-hook
+          (lambda ()
+            (abbrev-mode 1)
+            (auto-fill-mode 1)
+            (if (eq window-system 'x)
+                (font-lock-mode 1))))
+
 ;; magit for git
 (global-set-key (kbd "C-x g") 'magit-status)
 
